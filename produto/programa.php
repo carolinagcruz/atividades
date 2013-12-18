b14a339825b201b9b0e29aaf9ace24b1d284c5a0
carolinagcruz <karolguimaraes17@hotmail.com>
2013-12-18 16:17:40 -0200

    Create programa.php

---
+++ produto/programa.php
@@ -0,0 +1,22 @@
+<?php
+$codigo = 0;
+
+$codigo = $_POST["codigo"];
+
+
+switch ($codigo){
+	case 1:
+		echo "caneta 1.20";
+		break;
+	case 2:
+		echo "lápis 0.80";
+		break;
+	case 3:
+		echo "caderno 4.50";
+		break;
+	default;
+		echo "produto não cadastrado";
+
+}
+
+?>
