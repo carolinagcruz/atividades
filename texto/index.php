4415de73fe9e123fabf6c49a914726a4c169dd1d
carolinagcruz <karolguimaraes17@hotmail.com>
2013-12-18 16:22:24 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$x = 21;
+$y = 31;
+
+
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto = "O dia de hoje é ";
+
+echo $texto . $dia. "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "O dia de hoje é $dia/$mes/$ano";
+echo "<br/>";
+echo "$x + $y = " . ($x + $y);
+
+
+
+?>
