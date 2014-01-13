740e21c3d8f8b4354a4887aaccc71285c442b0aa
carolinagcruz <karolguimaraes17@hotmail.com>
2014-01-13 18:36:50 -0200

    Create campos_produto.php

---
+++ avaliacao-3/campos_produto.php
@@ -0,0 +1,23 @@
+<label>Nome do produto:</label>
+<input type="text" name="nomeProduto" />
+<br><br>
+<input type="radio" name="cor" value= "lilás" checked="checked"/>
+<label>Lilás</label>
+<input type="radio" name="cor" value= "vermelho"/>
+<label>Vermelho</label>
+<input type="radio" name="cor" value= "amarelo"/>
+<label>Amarelo</label>
+<br><br>
+<input type="checkbox" name="produtoNovo" value="sim" />
+<label>Poduto novo</label>
+<br><br>
+<label>Prazo de entrega</label>
+<select name="prazoEntrega">
+    <option>5 dias</option>
+    <option>10 dias</option>
+    <option>15 dias</option>
+</select>
+<br><br>
+<label>Detalhe do produto:</label>
+<br>
+<textarea name="detalheProduto"></textarea>
