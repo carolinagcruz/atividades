f87f403a770e201c096edbeca6488c9a7c72e04c
carolinagcruz <karolguimaraes17@hotmail.com>
2014-01-13 18:38:08 -0200

    Create form_cadastrar.php

---
+++ avaliacao-3/form_cadastrar.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Cadastro de Produtos</title>
+    </head>
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>
+        <form action="cadastrar.php" method="post">
+            <?php require_once("campos_produto.php"); ?><br><br>
+            <input type= "submit" value="Enviar" />
+        </form>
+        
+    </body>
+    
+</html>
